4feec9325f53eb37a54bc718c039d3b22feda614
DaffaHabibullah <daffahabibullah1@gmail.com>
2022-12-29 23:46:34 +0700

    Login add Login Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container, Center, Text, Stack, Input, Button, Link } from '@chakra-ui/react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <Container>
+      <Center display='flex' height='652px' justifyContent='center' alignItems='center' flexDirection='column' gap='20px'>
+        <Text fontSize='32px' as='b'>ChatYuk</Text>
+        <Text fontSize='24px'>Login</Text>
+        <Stack spacing='3'>
+          <Input height='48px' width='322px' placeholder='Alamat Email' />
+          <Input height='48px' width='322px' placeholder='Password' />
+        </Stack>
+        <Button height='48px' width='155px' colorScheme='blue'>Sign In</Button>
+        <Text fontSize='14px'>Tidak punya akun? <Link color='#9C30B7' href='#'>Daftar Sekarang</Link></Text>
+      </Center>
+    </Container>
   )
 }
 
