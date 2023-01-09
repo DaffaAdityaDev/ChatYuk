dc2b9eb4db667353220e6c544dd3e9954d342f26
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-01-10 04:48:58 +0700

    update login page design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Center, Text, Stack, Input, Button, FormControl } from '@chakra-ui/react'
+import { Container, Center, Text, Stack, Input, Button, FormControl, Box } from '@chakra-ui/react'
 import { useNavigate, Link } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebase';
@@ -30,23 +30,37 @@ function Login() {
         navigate ('/')
         console.log("test")
     }
+
+  
   return (
-    <Container>
-      <Center display='flex' height='100vh' justifyContent='center' alignItems='center' flexDirection='column' gap='20px'>
-        <Text fontSize='32px' as='b'>ChatYuk</Text>
-        <Text fontSize='24px'>Login</Text>
-        <form onSubmit={handleSubmit}>
-          <FormControl>
-            <Stack spacing='3'>
-              <Input type="email" height='48px' width='322px' placeholder='Alamat Email' />
-              <Input type="password" height='48px' width='322px' placeholder='Password' />
-            </Stack>
-            <Button height='48px' width='155px' colorScheme='blue' type='submit'>Sign In</Button>
-          </FormControl>
-        </form>
-        <Text fontSize='14px'>Tidak punya akun? <Link to='/register'>Daftar Sekarang</Link></Text>
+    <Box bgGradient='linear(to-r, #009FFF, #ec2F4B)' >
+      <Center display='flex' height='100vh' width="100vw" justifyContent='center' alignItems='center' flexDirection='column'>
+        <Box w="400px" h="fit-content" bgColor="rgba(255,255,255, 0.15)" blur="5px">
+          <Box backdropFilter='auto' backdropBlur='20px'>
+            <Center display="flex" flexDir="column">
+              <Text fontSize='32px' as='b' color="white">ChatYuk</Text>
+                <Text fontSize='24px' mb="1rem" color="white">Login</Text>
+                <form onSubmit={handleSubmit}>
+                  <FormControl>
+                    <Stack spacing='3'>
+                      <Input type="email" height='48px' width='322px' placeholder='Alamat Email' variant="filled"/>
+                      <Input type="password" height='48px' width='322px' placeholder='Password' variant="filled"/>
+                    </Stack>
+                    <Center pt="2rem">
+                      <Button height='48px' width='155px' colorScheme='blue' type='submit'>Sign In</Button>
+                    </Center>
+                  </FormControl>
+                </form>
+              <Text fontSize='14px' mt="1rem" mb="1rem" color="white">Tidak punya akun? 
+                <Text as='b' color="teal.200">
+                  <Link to='/register'> Daftar Sekarang</Link>
+                </Text > 
+                </Text>
+            </Center>
+          </Box>
+        </Box>
       </Center>
-    </Container>
+    </Box>
   )
 }
  
